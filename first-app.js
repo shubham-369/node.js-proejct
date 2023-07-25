272ffb5ad2b213339948765ba3c2ab48d1c0bb8d
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-07-25 15:47:54 +0530

    http request handle

--- first-app.js
+++ first-app.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello node.js");
+    if(req.url == "/"){
+        res.end("Hello there");
+    }else if(req.url == "/home"){
+        res.end("<h1>Welcome to homepage</h1>");
+    }else if(req.url == "/about"){
+        res.end("<h1>Welcome to about us page</h1>");
+    }else if(req.url == "/node"){
+        res.end("<h1>Welcome to the Node.js project</h1>");
+    }else{
+        res.writeHead(404,{"Content-type" : "text/Html"});
+        res.end("<h1>404 error page doesn't exist</h1>");
+    }
 });
 
 server.listen(4000,'127.0.0.1',() =>{
